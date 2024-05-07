4762962e17ac8d072eeccba14145cf81f2717983
almusfuad <almusfuad@gmail.com>
2024-05-07 19:47:51 +0600

    includes search query

--- ui/js/custom/manage_product.js
+++ ui/js/custom/manage_product.js
@@ -1,20 +1,57 @@
 var productModal = $('#productModal');
+
+
 $(function() {
+
+    // Function to render UI
+    function renderProducts(products) {
+        var table = '';
+        $.each(products, function(index, value) {
+            table += '<tr data-id="' + value.product_id + '" data-name="' + value.name + '" data-unit="' + value.uom_id + '" data-price="' + value.price_per_unit + '">' +
+                '<td>' + value.name + '</td>' +
+                '<td>' + value.uom_id + '</td>' +
+                '<td>' + value.price_per_unit + '</td>' +
+                '<td><span class="btn btn-xs btn-danger delete-product">Delete</span></td></tr>';
+        });
+        $("table").find('tbody').empty().html(table);
+    }
 
     // JSON data by api call
     $.get(productListApiUrl, function(response) {
         if (response && response.products) {
-            var table = '';
-            $.each(response.products, function(index, value) {
-                table += '<tr data-id="' + value.product_id + '" data-name="' + value.name + '" data-unit="' + value.uom_id + '" data-price="' + value.price_per_unit + '">' +
-                    '<td>' + value.name + '</td>' +
-                    '<td>' + value.uom_id + '</td>' +
-                    '<td>' + value.price_per_unit + '</td>' +
-                    '<td><span class="btn btn-xs btn-danger delete-product">Delete</span></td></tr>';
+            console.log(response.products);
+            renderProducts(response.products);
+
+            // Search Product
+            $('#searchProduct').on("input", function() {
+                var searchQuery = $(this).val().toLowerCase();
+                console.log(searchQuery);
+                // Filter products based on search query
+                var filteredProducts = response.products.filter(function(product) {
+                    return product.name.toLowerCase().includes(searchQuery);
+                });
+                // render the filtered products
+                renderProducts(filteredProducts);
             });
-            $("table").find('tbody').empty().html(table);
         }
     });
+
+
+
+});
+
+
+// delete product
+$(document).on("click", ".delete-product", function() {
+    var tr = $(this).closest('tr');
+    var data = {
+        product_id: tr.data('id')
+    };
+    var isDelete = confirm("Are you sure to delete " + tr.data('name') + " item?");
+    console.log(data);
+    if (isDelete) {
+        callApi("POST", productDeleteApiUrl, data);
+    }
 });
 
 
@@ -45,18 +82,10 @@ $('#saveProduct').on("click", function() {
     console.log(requestPayload);
 });
 
+// Search product
+$('#searchProduct').on("click", function() {
 
-// delete product
-$(document).on("click", ".delete-product", function() {
-    var tr = $(this).closet('tr');
-    var data = {
-        product_id: tr.data('id')
-    };
-    var isDelete = confirm("Are you sure to delete " + tr.data('name') + " item?");
-    if (isDelete) {
-        callApi("POST", productDeleteApiUrl, data);
-    }
-});
+})
 
 // Modal operation
 productModal.on('show.bs.modal', function() {
